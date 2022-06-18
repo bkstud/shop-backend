be1d900f820e8e77f6655bd89536e088b2125cd4
Bazyli Kot <ciobab97@gmail.com>
2022-06-18 15:18:30 +0200

    block empty name for item, update implemented

--- api/v1/controller/item.go
+++ api/v1/controller/item.go
@@ -18,7 +18,7 @@ func findItemById(c *gin.Context, id string) *model.Item {
 	result := db.First(&item, id)
 	if errors.Is(result.Error, gorm.ErrRecordNotFound) {
 		c.JSON(http.StatusNotFound,
-			fmt.Sprintf("Item with id %s not found", id))
+			gin.H{"error": fmt.Sprintf("Item with id %s not found", id)})
 		return nil
 
 	}
@@ -35,10 +35,17 @@ func GetItems(c *gin.Context) {
 // POST /items
 func CreateItem(c *gin.Context) {
 	item := new(model.Item)
+	// TODO: To reconsider if 'Name' field can be empty
 	if err := c.Bind(item); err != nil {
 		c.JSON(http.StatusBadRequest, err.Error())
+		return
 	}
-	db.Create(item)
+
+	if result := db.Create(item); result.Error != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": result.Error})
+		return
+	}
 	c.JSON(http.StatusOK, item)
 }
 
@@ -47,6 +54,17 @@ func EditItem(c *gin.Context) {
 	id := c.Param("id")
 	item := findItemById(c, id)
 	if item == nil {
+		return
+	}
+	newItem := new(model.Item)
+	if err := c.Bind(newItem); err != nil {
+		c.JSON(http.StatusBadRequest, err.Error())
+		return
+	}
+
+	if result := db.Model(&item).Updates(newItem); result.Error != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": result.Error})
 		return
 	}
 
@@ -60,6 +78,11 @@ func DeleteItem(c *gin.Context) {
 	if item == nil {
 		return
 	}
-	db.Delete(&item)
+
+	if result := db.Delete(&item); result.Error != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": result.Error})
+		return
+	}
 	c.JSON(http.StatusOK, item)
 }
--- api/v1/model/item.go
+++ api/v1/model/item.go
@@ -4,8 +4,9 @@ import "gorm.io/gorm"
 
 type Item struct {
 	gorm.Model
-	Name        string
+	// will not allow name to be empty string
+	Name        string `gorm:"not null;type:varchar(100);default:null"`
 	Description string
-	//TODO Find a way to limit possibilites to available, sold
+	// TODO Find a way to limit possibilites to available, sold
 	Status string
 }
