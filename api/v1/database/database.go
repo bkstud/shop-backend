29870cbaf7ead4b822c806f3261cfbbb123090b3
Bazyli Kot <ciobab97@gmail.com>
2022-06-19 16:38:41 +0200

    recreate db every rerun, ignore file add

--- api/v1/database/database.go
+++ api/v1/database/database.go
@@ -1,17 +1,26 @@
 package database
 
 import (
+	"os"
 	"shop/api/v1/model"
 
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
 
-var Database *gorm.DB = nil
+var (
+	Database *gorm.DB = nil
+	DB_NAME           = "shop.db"
+)
 
 func init() {
+	// Recreate every time as this will also happen in the container.
+	// Do not use on production.
+	if _, err := os.Stat(DB_NAME); err == nil {
+		os.Remove(DB_NAME)
+	}
 
-	db, err := gorm.Open(sqlite.Open("shop.db"), &gorm.Config{})
+	db, err := gorm.Open(sqlite.Open(DB_NAME), &gorm.Config{})
 	if err != nil {
 		panic("failed to connect database")
 	}
