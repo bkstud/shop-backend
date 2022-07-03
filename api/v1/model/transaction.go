6b537d3bdec63f15f7197e19feb06957833664eb
Bazyli Kot <ciobab97@gmail.com>
2022-07-03 16:46:09 +0200

    repeated or no user protection

--- api/v1/model/transaction.go
+++ api/v1/model/transaction.go
@@ -6,7 +6,7 @@ type Transaction struct {
 	gorm.Model
 	ItemID  uint
 	Item    Item
-	UserID  uint
+	UserID  uint `gorm:"not null;type:varchar(100);default:null"`
 	User    User
 	Payment string // TODO: To be updated when stripe is added
 	Type    string // purchase or return
